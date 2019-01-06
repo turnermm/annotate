9d6af93af15ae43ecfe2515d7fef0a1736c0cd4a
Myron Turner <turnermm02@shaw.ca>
2019-01-06 15:54:27 -0600

    Adds onlick handler for opening annotations, giving it immediate response time.

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
  jQuery( document ).ready(function() {    
   var last_anno_open; 
-   jQuery("span.anno") .mouseover(function() {
-       
+   jQuery("span.anno") .mouseover(function() {       
       var title = jQuery( this ).attr('title');     
-      setTimeout(function() {
-      if(last_anno_open)     {
-          jQuery(last_anno_open) .css('display','none');
-       }
-       last_anno_open = "span." + title;
-       jQuery("span." + title).css('display','inline-block');           
-       }, 1200);         
+      open_anno(title,1200);
     });
-	
+    
+  jQuery("span.anno") .click(function() {       
+      var title = jQuery( this ).attr('title');     
+      open_anno(title,1);
+    });
+    
+    function open_anno(title,tm) {
+       setTimeout(function() {
+            if(last_anno_open)     {
+              jQuery(last_anno_open) .css('display','none');
+           }
+           last_anno_open = "span." + title;
+           jQuery("span." + title).css('display','inline-block');           
+           }, tm);         
+    }
+    
     jQuery("span.annotation").click(function() {     
 	 jQuery(this).css('display','none');
    });	
