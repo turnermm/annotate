d45346c528c2fc4d54d4110d82987fec7e33aca7
Myron Turner <turnermm02@shaw.ca>
2019-01-18 14:49:00 -0600

    Adds support for DowkuWiki Quoting

--- syntax/anno.php
+++ syntax/anno.php
@@ -255,7 +255,9 @@ ol ol ol ol ol { list-style-type: lower-roman; }
         return $retv;
     },$html);
 
-
+$html = str_replace('<blockquote>', '<br /><span class = "anno_blockquote">',$html);
+$html = str_replace('</blockquote>', '</span>',$html);
+//msg($html);
 		$html = preg_replace('/<\/?div.*?>/ms',"",$html);
 		$html = preg_replace('/<!--.*?-->/ms',"",$html);	
      
